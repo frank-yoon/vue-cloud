baeb1c877242d4a759c4f8e3ae0ddded2ffe2079
Frank Yoon <frank.yoon@accenturefederal.com>
2020-02-03 15:58:24 -0500

    Updated unit test for Vuetify hello-world.

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -7,10 +7,31 @@ Vue.use(Vuetify);
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      data: () => ({
+        ecosystem: [
+          {
+            text: "vuetify-loader",
+            href: "https://github.com/vuetifyjs/vuetify-loader"
+          }
+        ],
+        importantLinks: [
+          {
+            text: "Documentation",
+            href: "https://vuetifyjs.com"
+          }
+        ],
+        whatsNext: [
+          {
+            text: "Explore components",
+            href: "https://vuetifyjs.com/components/api-explorer"
+          }
+        ]
+      })
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).toMatch("vuetify-loader");
+    expect(wrapper.text()).toMatch("Documentation");
+    expect(wrapper.text()).toMatch("Explore components");
+    expect(wrapper.text()).not.toMatch("ipso lorem");
   });
 });
